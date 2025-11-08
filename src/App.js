168d95e8686fddde3536b734854ba96df3522573
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-11-08 22:50:39 +0530

    EventBind

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Message from "./components/Message"
 import Counter from "./components/Counter"
 import FunctionalClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick'
+import EventBind  from "./components/EventBind"
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
@@ -20,8 +22,10 @@ function App() {
      <Welcome />
      <Message />
      <Counter/> */}
-     <FunctionalClick />
-     <ClassClick />
+     {/* <FunctionalClick />
+     <ClassClick /> */}
+     {/* <EventBind /> */}
+     <ParentComponent />
     </div>
   
   );
