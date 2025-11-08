185a555204d394e58a438fafff7755674d70bfdd
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-11-08 20:27:38 +0530

    FunctionClick and ClassClick

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Greet from "./components/Greet"
 import Welcome from "./components/Welcome"
 import Message from "./components/Message"
 import Counter from "./components/Counter"
+import FunctionalClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick'
 
 function App() {
   return (
     <div className="App">
-     <Greet name="Bruce" heroName="BatMan">
+     {/* <Greet name="Bruce" heroName="BatMan">
       <p> This is a children prop </p>
      </Greet>
      <Greet name="Clark" heroName="SuperMan">
@@ -17,7 +19,9 @@ function App() {
      <Greet name="Diana" heroName="Wonder Woman" />
      <Welcome />
      <Message />
-     <Counter/>
+     <Counter/> */}
+     <FunctionalClick />
+     <ClassClick />
     </div>
   
   );
