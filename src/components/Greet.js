88cd0049d5ea5353736eea819d4a0e769c308fdc
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-11-08 20:03:44 +0530

    props

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,11 +4,11 @@ import React from "react"
 //     return <h1>Hello Esheta!</h1>
 // }
 
-const Greet = (props) => {
+const Greet = ({name,heroName,children}) => {
     return (
        <div>
-         <h1>Hello {props.name} a.k.a {props.heroName}</h1>
-         {props.children}
+         <h1>Hello {name} a.k.a {heroName}</h1>
+         {children}
        </div>
     )
 }
