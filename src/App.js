d94167f79180da2ec1f54fdacc5d9d45df7e28df
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-11-08 17:07:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from "./components/Greet" 
+import Welcome from "./components/Welcome"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Greet name="Bruce" heroName="BatMan">
+      <p> This is a children prop </p>
+     </Greet>
+     <Greet name="Clark" heroName="SuperMan">
+      <button>Children Prop</button>
+     </Greet>
+
+     <Greet name="Diana" heroName="Wonder Woman" />
+     <Welcome />
     </div>
   );
 }
