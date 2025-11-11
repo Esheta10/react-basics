0332c6e248fbc5e40501b3c30f436bd8f0176647
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-11-11 13:33:57 +0530

    Lidecycle Update Method

--- src/components/LifecycleB.js
+++ src/components/LifecycleB.js
@@ -15,10 +15,20 @@ class LifecycleB extends Component {
   componentDidMount() {
     console.log("LifecycleB componentDidMount");
   }
+  shouldComponentUpdate(){
+    console.log("LifecycleB shouldComponentUpdate");
+    return true;
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log("LifecycleB getSnapshotBeforeUpdate");
+    return null;
+  }
+  componentDidUpdate(){
+    console.log("LifecyleB componentDidUpdate");
+  }
   render() {
     console.log("LifecycleB render");
     return <div>LifecycleB</div>;
   }
 }
-
 export default LifecycleB;
