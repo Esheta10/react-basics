2720690ffdc94f88030d5b4b4f4c3a3f11f47c6a
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-11-11 12:26:01 +0530

    Updating Lifecycle Component

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -1,24 +1,30 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
+import LifecycleB from "./LifecycleB";
 
-class LifecycleA extends component{
-    constructor(props){
-        super(props)
-        this.state = {
-            name: 'Vishwas'
-        }
-        console.log("LifecycleA constructor")
-    }
-    static getDerivedStateFromProps(props,state){
-        console.log("LifecycleA getDerivedStateFromProps")
-        return null
-    }
-    render(){
-        return(
-            <div>
-
-            </div>
-        )
-    }
+class LifecycleA extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Vishwas",
+    };
+    console.log("LifecycleA constructor");
+  }
+  static getDerivedStateFromProps(props, state) {
+    console.log("LifecycleA getDerivedStateFromProps");
+    return null;
+  }
+  componentDidMount() {
+    console.log("LifecycleA componentDidMount");
+  }
+  render() {
+    console.log("LifecycleA render");
+    return (
+         <div>
+           <div>LifecycleA</div>
+            <LifecycleB />
+        </div>
+    )
+  }
 }
 
-export default LifecycleA
+export default LifecycleA;
