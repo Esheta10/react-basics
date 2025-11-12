98c054c157e8d7979eb16c0864249d1506ab62a7
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-11-12 17:20:38 +0530

    Error Boundary

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import './App.css';
-import Greet from "./components/Greet" 
-import Welcome from "./components/Welcome"
-import Message from "./components/Message"
-import Counter from "./components/Counter"
-import FunctionalClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick'
-import EventBind  from "./components/EventBind"
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from "./components/NameList"
-import StyleSheet from './components/StyleSheet';
-import Inline from './components/Inline'
-import './appStyles.css'
-import styles from './appStyles.module.css'
-import Form from "./components/Form"
-import LifecycleA from './components/LifecycleA';
-import FragmentDemo from './components/FragmentDemo';
-import Columns from './components/Columns';
-import PureComp from './components/PureComp';
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
+import FunctionalClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+import EventBind from "./components/EventBind";
+import ParentComponent from "./components/ParentComponent";
+import UserGreeting from "./components/UserGreeting";
+import NameList from "./components/NameList";
+import StyleSheet from "./components/StyleSheet";
+import Inline from "./components/Inline";
+import "./appStyles.css";
+import styles from "./appStyles.module.css";
+import Form from "./components/Form";
+import LifecycleA from "./components/LifecycleA";
+import FragmentDemo from "./components/FragmentDemo";
+import Columns from "./components/Columns";
+import PureComp from "./components/PureComp";
 import RegComp from "./components/RegComp";
-import ParentComp from './components/ParentComp';
-import RefsDemo from './components/RefsDemo';
-import Input from './components/Input';
-import FocusInput from './components/FocusInput';
-import FRParentInput from './components/FRParentInput';
-import PortalDemo from './components/PortalDemo';
+import ParentComp from "./components/ParentComp";
+import RefsDemo from "./components/RefsDemo";
+import Input from "./components/Input";
+import FocusInput from "./components/FocusInput";
+import FRParentInput from "./components/FRParentInput";
+import PortalDemo from "./components/PortalDemo";
+import Hero from "./components/Hero";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
     <div className="App">
-     {/* <Greet name="Bruce" heroName="BatMan">
+      {/* <Greet name="Bruce" heroName="BatMan">
       <p> This is a children prop </p>
      </Greet>
      <Greet name="Clark" heroName="SuperMan">
@@ -40,28 +42,36 @@ function App() {
      <Welcome />
      <Message />
      <Counter/> */}
-     {/* <FunctionalClick />
+      {/* <FunctionalClick />
      <ClassClick /> */}
-     {/* <EventBind /> */}
-     {/* <ParentComponent /> */}
-     {/* <UserGreeting /> */}
-     {/* <NameList /> */}
-     {/* <StyleSheet primary={true} /> */}
-     {/* <Inline /> */}
-     {/* <h1 className='error'>Error</h1>
+      {/* <EventBind /> */}
+      {/* <ParentComponent /> */}
+      {/* <UserGreeting /> */}
+      {/* <NameList /> */}
+      {/* <StyleSheet primary={true} /> */}
+      {/* <Inline /> */}
+      {/* <h1 className='error'>Error</h1>
      <h1 className={styles.success}>Success</h1> */}
-     {/* <Form /> */}
-     {/* <LifecycleA/> */}
-     {/* <FragmentDemo /> */}
-     {/* <Columns /> */}
-     {/* <ParentComp /> */}
-     {/* <RefsDemo /> */}
-     {/* <Input /> */}
-     {/* <FocusInput /> */}
-    {/* <FRParentInput/> */}
-    <PortalDemo />
+      {/* <Form /> */}
+      {/* <LifecycleA/> */}
+      {/* <FragmentDemo /> */}
+      {/* <Columns /> */}
+      {/* <ParentComp /> */}
+      {/* <RefsDemo /> */}
+      {/* <Input /> */}
+      {/* <FocusInput /> */}
+      {/* <FRParentInput/> */}
+      {/* <PortalDemo /> */}
+      <ErrorBoundary>
+        <Hero heroName="BatMan" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+            <Hero heroName="SuperMan" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Joker" />
+      </ErrorBoundary>
     </div>
-  
   );
 }
 
